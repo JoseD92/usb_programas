0195d3f872fa7f376bb9a577f780eed955c33e25
josed92 <jose_daniel_d@hotmail.com>
2018-03-05 17:27:10 -0400

    add ep program, makefile uses pdftk instead of pdfunite and nums is not background

--- makefile
+++ makefile
@@ -2,7 +2,8 @@ all: programa.pdf
 
 programa.pdf: ./other/numeros.pdf
 	for i in *.doc; do libreoffice --headless --convert-to pdf $$i; done
-	pdfunite *.pdf programa0.pdf
+#	pdfunite *.pdf programa0.pdf
+	pdftk *.pdf cat output programa0.pdf
 	./other/addNumbers.sh programa0.pdf other/numeros.pdf programa.pdf
 	rm programa0.pdf
 	for i in *.doc; do rm "`echo "$$i" | cut -d'.' -f1`.pdf"; done
--- other/addNumbers.sh
+++ other/addNumbers.sh
@@ -8,30 +8,21 @@ nu_pages=`mawk '{print $2}' nu_pages.txt`
 # burst the page number file into its component pages
 pdftk $2 burst output nums_%04d.pdf
 
-# start converting pages from page 2
-x=1 # initialize x
+x=1
 
 # place the page numbers on each page of newbook
 while [ "$x" -le "$nu_pages" ]; do
+    if [ "$x" -lt 10 ]; then
+      pdftk nums_000"$x".pdf background book_000"$x".pdf output fin_000"$x".pdf
+    elif [ "$x" -lt 100 ]; then
+      pdftk nums_00"$x".pdf background book_00"$x".pdf output fin_00"$x".pdf
+    elif [ "$x" -lt 1000 ]; then
+      pdftk nums_0"$x".pdf background book_0"$x".pdf output fin_0"$x".pdf
+    fi
 
-     if [ "$x" -lt 10 ]; then
-    pdftk book_000"$x".pdf background nums_000"$x".pdf output fin_000"$x".pdf
-         echo "Finished page $x of $nu_pages."
-         x=$(($x+1))
-
-     elif [ "$x" -lt 100 ]; then
-    pdftk book_00"$x".pdf background nums_00"$x".pdf output fin_00"$x".pdf
-         echo "Finished page $x of $nu_pages."
-         x=$(($x+1))
-
-     elif [ "$x" -lt 1000 ]; then
-    pdftk book_0"$x".pdf background nums_0"$x".pdf output fin_0"$x".pdf
-         echo "Finished page $x of $nu_pages."
-         x=$(($x+1))
-
-     fi
+    echo "Finished page $x of $nu_pages."
+    x=$(($x+1))
 done
-
 
 # create the new PDF file and move it to the original directory
 pdftk  fin_*.pdf cat output $3
